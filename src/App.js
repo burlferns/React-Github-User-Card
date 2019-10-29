0a4ab1a3ef322eb1fd2bfaee42df62f23d9e73c2
std <std@admins-Mac-mini.local>
2019-10-29 13:38:06 -0700

    header added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import styled from "styled-components";
+
+import Header from "./components/Header";
+
+const StylDiv = styled.div`
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <StylDiv>
+      <Header/>
 
 
-    </div>
+    </StylDiv>
   );
 }
 
