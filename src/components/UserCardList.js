a637e14db2305465d7f30b2705759ea63cb43727
std <std@admins-Mac-mini.local>
2019-10-30 07:35:09 -0700

    interesting point

--- src/components/UserCardList.js
+++ src/components/UserCardList.js
@@ -4,7 +4,11 @@ import axios from 'axios';
 
 import UserCard from "./UserCard";
 
-const followerList = [ "tetondan", "dustinmyers", "justsml", "luishrd", "bigknell" ];
+// const followerList = [ "tetondan", "dustinmyers", "justsml", "luishrd", "bigknell" ];
+
+let compDidMount_count = 0;
+let compDidUpdate_count = 0;
+let render_count = 0;
 
 const StylPloading = styled.p`
   font-size: 30px;
@@ -15,38 +19,78 @@ const StylPloading = styled.p`
 class UserCardList extends React.Component {
   state = {
     myData : {},
+    followerList: [],
     followerData : [],
   }
 
   componentDidMount() {
+    console.log(`componentDidMount called and its count is: ${compDidMount_count}`);
+    compDidMount_count++;
+
+    // axios
+    //   .get('https://api.github.com/users/tetondan')
+    //   .then(res => {
+    //     console.log("This is myData from the server",res.data);
+    //     this.setState({
+    //       myData : res.data
+    //     });
+    //   })
+    //   .catch(err => console.log(err));
+
     axios
-      .get('https://api.github.com/users/burlferns')
+      .get('https://api.github.com/users/tetondan/followers')
       .then(res => {
-        console.log("This is myData from the server",res.data);
+        console.log("This is my followers from the server",res.data);
+        const folAry = res.data.map( elm => elm.url)
+        console.log("This is folAry data",folAry);  
         this.setState({
-          myData : res.data
+          followerList : folAry
         });
       })
       .catch(err => console.log(err));
 
-    followerList.forEach( elem => {
-      axios
-        .get(`https://api.github.com/users/${elem}`)
-        .then(res => {
-          console.log("This is followerData from the server",res.data);
-          this.setState({
-            followerData : [ ...this.state.followerData, res.data ]
-          });
-        })
-        .catch(err => console.log(err));
 
-    });
+    
+  }
+
+
+  componentDidUpdate() {
+    console.log(`componentDidUpdate called and its count is: ${compDidUpdate_count}`);
+    compDidUpdate_count++;
+
+    // if(followerData.length===0) {
+      // followerList.forEach( elem => {
+      //   axios
+      //     .get(`https://api.github.com/users/${elem}`)
+      //     .then(res => {
+      //       console.log("This is followerData from the server",res.data);
+      //       this.setState({
+      //         followerData : [ ...this.state.followerData, res.data ]
+      //       });
+      //     })
+      //     .catch(err => console.log(err));
+
+      // });
+
+    // }
+
+
+
+
   }
 
 
 
+
+
+
+
+
   render() {
-    const {myData,followerData} = this.state;
+    console.log(`render called and its count is: ${render_count}`);
+    render_count++;
+
+    const {myData,followerData,followerList} = this.state;
 
     // console.log("myData is:",myData);
     // console.log("followerData is:",followerData);
@@ -58,7 +102,7 @@ class UserCardList extends React.Component {
         : <UserCard data={myData} />
       }
 
-      {followerData.length!==followerList.length
+      { (followerList.length===0 || followerData.length!==followerList.length)
         ? <StylPloading>Loading follower data ...</StylPloading>
         : followerData.map( elem =>
           <UserCard data={elem} key={Date.now()} />
